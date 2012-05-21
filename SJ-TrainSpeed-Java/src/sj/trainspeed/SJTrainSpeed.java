d4dac9efc0ab6bb14a6fc952d4f92b04e9b3a4a3
Martin Thuresson <martin.thure@gmail.com>
2012-05-21 21:16:35 +0200

    Clean build

--- SJ-TrainSpeed-Java/src/sj/trainspeed/SJTrainSpeed.java
+++ SJ-TrainSpeed-Java/src/sj/trainspeed/SJTrainSpeed.java
@@ -13,8 +13,9 @@ public class SJTrainSpeed {
      */
     public static void main(String[] args) throws IOException, InterruptedException{
      InfoWindow userwindow = new InfoWindow();
-     //GetSpeed getter =  new GetSpeed("http://veolia.internet--access.net:8105/oxmap_ajax","SpeedOverGround",1.852);
-     //GetSpeed getter =  new GetSpeed("http://veolia.internet--access.net:8105/oxmap_ajax","SpeedOverGround",1.852);
+    
+     GetSpeed getter =  new GetSpeed("http://veolia.internet--access.net:8105/oxmap_ajax","SpeedOverGround",1.852);
+     //GetSpeed getter =  new GetSpeed("http://www.ombord.info/api/xml/position/", "speed", 3.6);
      userwindow.setVisible(true);
      while(true){
      userwindow.setSpeed(getter.getSpeed());
